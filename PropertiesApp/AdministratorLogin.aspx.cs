c80b387a27e24961cd09fad24d3f2608ae04e698
Juan Cajamarca <juancajamarca@outlook.es>
2020-10-09 16:24:35 -0700

    Administrator login done

--- PropertiesApp/AdministratorLogin.aspx.cs
+++ PropertiesApp/AdministratorLogin.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace PropertiesApp
 {
@@ -16,7 +19,48 @@ namespace PropertiesApp
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            Label1.Text = "The button was pressed!";
+            if (TextBox1.Text != "" && TextBox2.Text != "")
+            {
+                String password = GetAdministratorPassword();
+
+                if (password == "")
+                {
+                    Label1.Text = "No existen usuarios con el email ingresado.";
+                }
+                else
+                {
+                    ValidatePassword(password);
+                }
+            }
+            else
+            {
+                Label1.Text = "Por favor, ingrese email y contraseña.";
+            }
+        }
+
+        private String GetAdministratorPassword()
+        {
+            String realPassword;
+
+            SqlConnection vcon = new SqlConnection(ConfigurationManager.ConnectionStrings["PropertiesConnection"].ToString());
+            vcon.Open();
+            SqlCommand cmd = new SqlCommand("SELECT password FROM [dbo].[Administrator] WHERE email = '" + TextBox1.Text + "';", vcon);
+            realPassword = Convert.ToString(cmd.ExecuteScalar());
+            vcon.Close();
+            
+            return realPassword;
+        }
+
+        private void ValidatePassword(String password)
+        {
+            if (TextBox2.Text == password)
+            {
+                Label1.Text = "¡Iniciaste sesión!";
+            }
+            else
+            {
+                Label1.Text = "Contraseña incorrecta.";
+            }
         }
     }
 }
